022614e86deab21f2de25e8cb3dfc6716718d055
Klaudia <klaudia.krzywda@gmail.com>
2019-08-26 11:37:28 +0100

    Updates

---
+++ precache-manifest.8b93128efa90f67b9352666c21cd13ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a8fcca7cb0250762b65919dc9cf050a",
+    "url": "/Country-info/index.html"
+  },
+  {
+    "revision": "3a441c743262b13735f8",
+    "url": "/Country-info/static/css/main.a450c566.chunk.css"
+  },
+  {
+    "revision": "e56839eb30a7ff470b16",
+    "url": "/Country-info/static/js/2.a0cf9c8e.chunk.js"
+  },
+  {
+    "revision": "3a441c743262b13735f8",
+    "url": "/Country-info/static/js/main.ce562180.chunk.js"
+  },
+  {
+    "revision": "8a3fb79ecf0b96835787",
+    "url": "/Country-info/static/js/runtime~main.687043f5.js"
+  }
+]);
